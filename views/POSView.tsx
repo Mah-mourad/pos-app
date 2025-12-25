0712bfe98378e7a70a4e3197c61b31cfe19f4b59
Mah-mourad <mahmoudmourad249@gmail.com>
2025-12-26 01:14:23 +0200

    fix: remove handle_tool import from POSView for web build

--- views/POSView.tsx
+++ views/POSView.tsx
@@ -6,7 +6,15 @@ import ProductConfigModal from '../components/ProductConfigModal';
 import { usePOS } from '../context/POSContext';
 // import { Product, CartItem, PricingMethod, Service } from '../types';
 import { Product, CartItem, PricingMethod, Service, Category } from '../types';
-import { createTransaction } from '../handle_tool/transactions.service';
+// import { createTransaction } from '../handle_tool/transactions.service';
+
+let createTransaction: any;
+
+if ((window as any)?.electron) {
+  createTransaction =
+    require('../handle_tool/transactions.service').createTransaction;
+}
+
 
 const POSView: React.FC = () => {
   const { 
